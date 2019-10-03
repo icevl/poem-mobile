786255492db3f4b41d0cc71ff2648b7b3afafeda
Mikhail Kabanov <kabanov.mike@gmail.com>
2019-10-04 03:28:12 +1000

    Auth screen

--- AppMain.ts
+++ AppMain.ts
@@ -3,9 +3,11 @@ import { createStackNavigator } from 'react-navigation-stack';
 
 import FeedScreen from './apps/screens/feed/FeedScreen';
 import MenuScreen from './apps/screens/menu/Menu';
+import AuthScreen from './apps/screens/auth/AuthScreen';
 
 const AppNavigator = createStackNavigator(
     {
+        Auth: AuthScreen,
         Feed: FeedScreen,
         Menu: MenuScreen
     },
